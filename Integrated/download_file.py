4894747f499544a6d7e9fd8c10c27758430fefef
Aditya-aka-leo <leohaxter@gmail.com>
2023-01-17 11:44:22 +0530

    updates from local

--- Integrated/download_file.py
+++ Integrated/download_file.py
@@ -7,17 +7,34 @@ from flask import Flask, request
 from JsonExtractor import *
 from os import path
 from pydub import AudioSegment
+import os
+def del_files():
+    path = r"/home/ubuntu/Project-NoiseX/songs"
+    for file_name in os.listdir(path):
+        # construct full file path
+        file = path + file_name
+        if os.path.isfile(file):
+            print('Deleting file:', file)
+            os.remove(file)
 
+def call_models(filename):
+    s_finger_print=audio_finger_print(filename)
+    #print(s_genre)
+    print(s_finger_print)
+
+
+def mp3_to_wav(src,des):
+    sound = AudioSegment.from_mp3(src)
+    sound.export(des, format="wav")
 def download_from_s3(url):
+    del_files()
     import wget
     output_directory="/home/ubuntu/Project-NoiseX/songs"
     url=url
     filename = wget.download(url,out=output_directory)
+    src=output_directory+"/"+filename
+    mp3_to_wav(src,output_directory)
     print(filename)
-    #s_genre=genre(filename)
-    s_finger_print=audio_finger_print(filename)
-    #print(s_genre)
-    print(s_finger_print)    
     return "none"
 download_from_s3("https://noisex.s3.ap-south-1.amazonaws.com/songs/1673933965681.mp3")    
 
